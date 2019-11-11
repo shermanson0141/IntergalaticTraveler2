fc3b932d7df704dbc40197bf7de1f6c7906d3857
Severin Hermanson <shermanson0141@stu.d214.org>
2019-11-11 11:08:19 -0600

    added a random integer 1-3 to 2nd view controller

--- IntergalaticTraveler2/SecondViewController.swift
+++ IntergalaticTraveler2/SecondViewController.swift
@@ -11,21 +11,18 @@ import UIKit
 class SecondViewController: UIViewController {
 
     @IBOutlet weak var starImageView: UIImageView!
+    var starColor: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let randomNumber = Int.random(in:1...3)
+        let myImage = UIImage(named: "\(starColor)\(randomNumber)")
+        
+        starImageView.image = myImage
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
