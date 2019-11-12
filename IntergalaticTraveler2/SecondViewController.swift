8928e63baaa74968ecd3925da33bb0dfbe09712c
Severin Hermanson <shermanson0141@stu.d214.org>
2019-11-12 11:23:56 -0600

    added code to randomly display 3 blue and red images

--- IntergalaticTraveler2/SecondViewController.swift
+++ IntergalaticTraveler2/SecondViewController.swift
@@ -11,13 +11,14 @@ import UIKit
 class SecondViewController: UIViewController {
 
     @IBOutlet weak var starImageView: UIImageView!
-    var starColor: String!
+    var starColor: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let randomNumber = Int.random(in:1...3)
         let myImage = UIImage(named: "\(starColor)\(randomNumber)")
+        print("\(starColor)\(randomNumber)")
         
         starImageView.image = myImage
     }
--- IntergalaticTraveler2/ViewController.swift
+++ IntergalaticTraveler2/ViewController.swift
@@ -17,9 +17,17 @@ class ViewController: UIViewController {
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let nameOfColor = colorTextField.text!
+        if let data = colorTextField.text,data != "Blue and Red" {
         let NVC = segue.destination as! SecondViewController
-        NVC.starColor = nameOfColor
+        NVC.starColor = data
+        } else {
+            let alert = UIAlertController (title: "Error", message: "There was no data", preferredStyle: .alert)
+            let button = UIAlertAction (title: "Ok", style: .default, handler: nil)
+            alert.addAction(button)
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
     }
 }
 
