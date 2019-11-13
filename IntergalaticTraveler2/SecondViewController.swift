dd79e52500cb30a67c7ccfe093e7d4f0460c9145
Severin Hermanson <shermanson0141@stu.d214.org>
2019-11-13 11:39:53 -0600

    cleaned up code

--- IntergalaticTraveler2/SecondViewController.swift
+++ IntergalaticTraveler2/SecondViewController.swift
@@ -9,21 +9,23 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     @IBOutlet weak var starImageView: UIImageView!
+    
     var starColor: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let randomNumber = Int.random(in:1...3)
+        
         let myImage = UIImage(named: "\(starColor)\(randomNumber)")
+        
         self.navigationItem.title = "\(starColor)\(randomNumber)"
         
         starImageView.image = myImage
     }
     
-
     
-
+    
 }
